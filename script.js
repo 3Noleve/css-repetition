a7535371e88a7e47f6280d99696cb910a46892b3
3Noleve <prohoreddinson@gmail.com>
2023-07-13 15:42:32 +0300

    feat: Add close sidebar functionality

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleSidebar() {
+function openSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("sidebar-open");
+}
+
+function closeSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("sidebar-close");
 }
